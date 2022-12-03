dd1bd601d7f8068c48599395657ba78cd51279f3
sumkyun <tndyd0706@naver.com>
2022-12-03 13:47:17 +0900

    송섬균의 테스트

---
+++ src/test/java/com/thenine/hrmsystem/sumkyunTest.java
@@ -0,0 +1,16 @@
+package com.thenine.hrmsystem;
+
+import com.thenine.hrmsystem.repository.HumanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+public class sumkyunTest {
+
+    //asd
+}
