b2082e3013ed074bf18fc256d9e8c898a6920a80
redjen8 <seokwoo2525@gmail.com>
2022-12-05 00:13:27 +0900

    20221205 Feat : Human 도메인 및 이름으로 조회 메서드 추가

--- src/main/java/com/thenine/hrmsystem/repository/HumanRepository.java
+++ src/main/java/com/thenine/hrmsystem/repository/HumanRepository.java
@@ -1,9 +1,11 @@
 package com.thenine.hrmsystem.repository;
 
+import com.thenine.hrmsystem.domain.Human;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class HumanRepository {
@@ -14,7 +16,14 @@ public class HumanRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<String> getHumanNameList() {
-        return jdbcTemplate.queryForList("SELECT name FROM test", String.class);
+    public Optional<Human> getHumanByName(String name) {
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject("SELECT * FROM Human WHERE name = ?",
+                        (rs, rowNum) -> Human.builder()
+                                .ID(rs.getBigDecimal("ID").longValue())
+                                .name(rs.getString("name"))
+                                .birthDate(rs.getDate("birth_date"))
+                                .academicBackGround(rs.getInt("academic_background"))
+                                .build(), name));
     }
 }
--- src/test/java/com/thenine/hrmsystem/HumanTest.java
+++ src/test/java/com/thenine/hrmsystem/HumanTest.java
@@ -1,11 +1,13 @@
 package com.thenine.hrmsystem;
 
+import com.thenine.hrmsystem.domain.Human;
 import com.thenine.hrmsystem.repository.HumanRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
@@ -17,8 +19,9 @@ public class HumanTest {
     private HumanRepository humanRepository;
 
     @Test
-    void 사람_이름_전부_조회 () {
-        List<String> nameList = humanRepository.getHumanNameList();
-        assertNotEquals(nameList.size(), 0);
+    void 이름으로_조회() {
+        final String testName = "우유윤";
+        Optional<Human> testHuman = humanRepository.getHumanByName(testName);
+        testHuman.ifPresent(human -> assertEquals(human.getName(), testName));
     }
 }
